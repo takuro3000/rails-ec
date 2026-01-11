7696bcdfbfaaac82b92220387d42279db0de9214
takuro3000 <talangxiaotian6@gmail.com>
2026-01-11 14:48:37 +0900

    コントローラー作成

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_path, notice: "会員登録が完了しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
